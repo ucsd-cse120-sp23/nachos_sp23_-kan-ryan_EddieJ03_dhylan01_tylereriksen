e19dfecd1fa6ad61d7cf6aad117a3987b4f0afc4
Edward C Jin <ecjin@ieng6-202.ucsd.edu>
2023-05-08 17:21:11 -0700

    added ppn management to UserKernel

--- nachos/userprog/UserKernel.java
+++ nachos/userprog/UserKernel.java
@@ -4,6 +4,8 @@ import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
 
+import java.util.*;
+
 /**
  * A kernel that can support multiple user processes.
  */
@@ -29,6 +31,12 @@ public class UserKernel extends ThreadedKernel {
 				exceptionHandler();
 			}
 		});
+
+		int physicalPageCount = Machine.processor().getNumPhysPages();
+
+		for (int i = 0; i < physicalPageCount; i++) {
+			this.freePPN.add(i);
+		}
 	}
 
 	/**
@@ -37,17 +45,17 @@ public class UserKernel extends ThreadedKernel {
 	public void selfTest() {
 		super.selfTest();
 
-		System.out.println("Testing the console device. Typed characters");
-		System.out.println("will be echoed until q is typed.");
+		// System.out.println("Testing the console device. Typed characters");
+		// System.out.println("will be echoed until q is typed.");
 
-		char c;
+		// char c;
 
-		do {
-			c = (char) console.readByte(true);
-			console.writeByte(c);
-		} while (c != 'q');
+		// do {
+		// 	c = (char) console.readByte(true);
+		// 	console.writeByte(c);
+		// } while (c != 'q');
 
-		System.out.println("");
+		// System.out.println("");
 	}
 
 	/**
@@ -119,9 +127,36 @@ public class UserKernel extends ThreadedKernel {
 		super.terminate();
 	}
 
+	public int acquirePPN() {
+		this.lock.acquire();
+
+		if (this.freePPN.isEmpty()) {
+			this.lock.release();
+			return -1;
+		} else {
+			int ppn = freePPN.pollFirst();
+			this.lock.release();
+			return ppn;
+		}
+	}
+
+	public void releasePPN(int physicalPageNumber) {
+		this.lock.acquire();
+
+		this.freePPN.add(physicalPageNumber);
+
+		this.lock.release();
+	}
+
 	/** Globally accessible reference to the synchronized console. */
 	public static SynchConsole console;
 
 	// dummy variables to make javac smarter
 	private static Coff dummy1 = null;
+
+	// free pages
+	LinkedList<Integer> freePPN = new LinkedList<>();
+
+	// lock
+	Lock lock = new Lock();
 }
