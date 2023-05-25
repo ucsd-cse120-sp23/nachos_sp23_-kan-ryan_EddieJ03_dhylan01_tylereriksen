f52598cda12f8f8b58c603ef9258370987deae37
Edward C Jin <ecjin@ieng6-201.ucsd.edu>
2023-05-24 20:13:53 -0700

    setup stuff in kernel for page swapping

--- nachos/vm/VMKernel.java
+++ nachos/vm/VMKernel.java
@@ -5,15 +5,47 @@ import nachos.threads.*;
 import nachos.userprog.*;
 import nachos.vm.*;
 
+import java.util.*;
+
 /**
  * A kernel that can support multiple demand-paging user processes.
  */
 public class VMKernel extends UserKernel {
+	private class InvertedPage {
+        public boolean pin;
+		public int vpn;
+		public UserProcess process;
+
+
+		public InvertedPage(boolean pin, int vpn, UserProcess process) {
+			this.pin = pin;
+			this.vpn = vpn;
+			this.process = process;
+		}
+	}
+
 	/**
 	 * Allocate a new VM kernel.
 	 */
 	public VMKernel() {
 		super();
+		
+		freeSwapAreas = new LinkedList<>();
+
+		swappingFile = ThreadedKernel.fileSystem.open("SwappingFile", true);
+
+		int numPhysPages = Machine.processor().getNumPhysPages();
+
+		invertedPageTable = new InvertedPage[numPhysPages];
+
+		for (int i = 0; i < numPhysPages; i++) {
+			freeSwapAreas.add(i);
+			invertedPageTable[i] = new InvertedPage(false, -1, null);
+		}
+
+		lock = new Lock();
+
+		condition = new Condition(lock);
 	}
 
 	/**
@@ -41,6 +73,7 @@ public class VMKernel extends UserKernel {
 	 * Terminate this kernel. Never returns.
 	 */
 	public void terminate() {
+        ThreadedKernel.fileSystem.remove("SwappingFile");
 		super.terminate();
 	}
 
@@ -48,4 +81,14 @@ public class VMKernel extends UserKernel {
 	private static VMProcess dummy1 = null;
 
 	private static final char dbgVM = 'v';
+
+	public static LinkedList<Integer> freeSwapAreas;
+
+	public static InvertedPage[] invertedPageTable;
+
+	public static OpenFile swappingFile;
+
+	public static Lock lock;
+
+	public static Condition condition;
 }
